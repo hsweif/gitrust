b45a4c321f811587a9e41b342f594157a09a14c5
Hsuan-Wei Fan <f.hsuanwei@gmail.com>
2024-12-11 18:16:35 +0800

    fix(cat-file): tree object parsing

--- src/commands/cat_file.rs
+++ src/commands/cat_file.rs
@@ -18,7 +18,12 @@ pub fn execute(command: Command) {
                 if size_flag {
                     println!("{}", object_size);
                 } else if contents_flag {
-                    println!("{}", content);
+                    if object_type == "tree" {
+                        println!("{}", parse_tree_object(&content));
+                    } else {
+                        let content = str::from_utf8(&content).expect("Invalid UTF-8");
+                        println!("{}", content);
+                    }
                 } else if type_flag {
                     println!("{}", object_type);
                 } else {
@@ -30,7 +35,7 @@ pub fn execute(command: Command) {
     }
 }
 
-fn parse_object(data: &[u8]) -> (usize, String, String) {
+fn parse_object(data: &[u8]) -> (usize, String, Vec<u8>) {
     // 1. Decompress the data with zlib
     let mut decoder = ZlibDecoder::new(data);
     let mut decompressed_data = Vec::new();
@@ -58,10 +63,49 @@ fn parse_object(data: &[u8]) -> (usize, String, String) {
             return (
                 size,
                 object_type_str,
-                String::from_utf8_lossy(content).to_string(),
+                content.iter().map(|&b| b).collect::<Vec<u8>>(),
             );
         }
     }
     // Return empty strings if parsing fails
-    (0, "".to_string(), "".to_string())
+    (0, String::new(), Vec::new())
+}
+
+fn parse_tree_object(data: &[u8]) -> String {
+    let mut i = 0;
+    let mut result = String::new();
+    while i < data.len() {
+        // Read mode
+        let mut mode_end = i;
+        while mode_end < data.len() && data[mode_end] != b' ' {
+            mode_end += 1
+        }
+        if mode_end == data.len() {
+            break;
+        }
+        let mode = &data[i..mode_end];
+
+        // Read filename
+        i = mode_end + 1;
+        let mut filename_end = i;
+        while data[filename_end] != 0 {
+            filename_end += 1;
+        }
+        let filename = &data[i..filename_end];
+
+        // Read hash
+        i = filename_end + 1;
+        let hash = &data[i..i + 20]; // SHA-1 hash is 20 bytes
+        i += 20;
+        // Convert hash to hex
+        let hash_hex = hash
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>();
+        let mode_str = str::from_utf8(mode).expect("Invalid mode");
+        let mode: u32 = mode_str.parse().expect("Invalid number");
+        let filename_str = str::from_utf8(filename).expect("Invalid filename");
+        result.push_str(&format!("{:06} {}  {}\n", mode, hash_hex, filename_str));
+    }
+    result
 }
