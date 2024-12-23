9b8723db0b1294a81b973920565e760d1b76032b
Hsuan-Wei Fan <f.hsuanwei@gmail.com>
2024-12-23 17:55:49 +0800

    refactor: use let-else pattern

--- src/objects/factory.rs
+++ src/objects/factory.rs
@@ -20,24 +20,23 @@ pub fn parse_object(data: &[u8]) -> Result<Box<dyn Object>, Box<dyn std::error::
     let header_end = decompressed_data.iter().position(|&b| b == 0).unwrap();
     let header = &decompressed_data[..header_end];
     let content = decompressed_data[header_end + 1..].to_vec();
-    if let Some(space_pos) = header.iter().position(|&b| b == b' ') {
-        let object_type = &header[..space_pos];
-        let _size = std::str::from_utf8(&header[space_pos + 1..])?.parse::<usize>()?;
-        let object_type = std::str::from_utf8(object_type)?;
-        match object_type {
-            "blob" => {
-                let blob = Blob::from_content(content)?;
-                Ok(Box::new(blob))
-            }
-            "tree" => {
-                let tree = Tree::from_content(content)?;
-                Ok(Box::new(tree))
-            }
-            "commit" | "tag" => unimplemented!(),
-            _ => Err("Invalid object type".into()),
+    let Some(space_pos) = header.iter().position(|&b| b == b' ') else {
+        return Err("Invalid header".into());
+    };
+    let object_type = &header[..space_pos];
+    let _size = std::str::from_utf8(&header[space_pos + 1..])?.parse::<usize>()?;
+    let object_type = std::str::from_utf8(object_type)?;
+    match object_type {
+        "blob" => {
+            let blob = Blob::from_content(content)?;
+            Ok(Box::new(blob))
         }
-    } else {
-        Err("Invalid header".into())
+        "tree" => {
+            let tree = Tree::from_content(content)?;
+            Ok(Box::new(tree))
+        }
+        "commit" | "tag" => unimplemented!(),
+        _ => Err("Invalid object type".into()),
     }
 }
 
